88d1d7a4b8b54dd8bef5e3d0356b92b5ded7888d
kpopino <david8.escola@gmail.com>
2025-07-02 18:46:19 +0000

    meu segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center, // Alinha verticalmente ao centro
           children: <Widget>[
             // Texto fixo explicando a funcionalidade
-            const Text('You have pushed the button this many times:'),
+            const Text('voce apertou o botão muitas vezes'),
 
             // Texto dinâmico que mostra o valor atual do contador
             Text(
